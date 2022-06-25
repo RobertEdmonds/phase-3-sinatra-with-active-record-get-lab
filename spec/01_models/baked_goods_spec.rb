e32ae7ea973ef3bb75c7d6004e0a2bbfcdef38b5
Robert Edmonds <bobby.edmonds89@gmail.com>
2022-06-25 15:36:11 -0600

    commit tree b752b3148e7b1e1c0a277516c7208ab6b6f97022

--- spec/01_models/baked_goods_spec.rb
+++ spec/01_models/baked_goods_spec.rb
@@ -5,12 +5,12 @@ describe BakedGood do
   before do
     bakery1 = Bakery.create(name: "Northside")
     bakery2 = Bakery.create(name: "Southside")
-    BakedGood.create(name: "Croissant", price: 3, bakery_id: bakery1.id)
+    BakedGood.create(name: "Croissant", price: 5, bakery_id: bakery1.id)
     BakedGood.create(name: "Bagel", price: 2, bakery_id: bakery2.id)
   end
   
   it "has the correct columns in the baked_goods table" do
-    expect(baked_good1).to have_attributes(name: "Croissant", price: 3, bakery_id: Bakery.first.id)
+    expect(baked_good1).to have_attributes(name: "Croissant", price: 5, bakery_id: Bakery.first.id)
   end
 
   it "knows about its associated bakery" do
